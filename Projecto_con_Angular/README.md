39178e58604e9a385ad19dcd8a5a08f5e071b4eb
Leandro Nykolajczuk <108647874+Leonyko@users.noreply.github.com>
2022-08-11 04:04:15 -0300

    borrar

--- Projecto_con_Angular/README.md
+++ Projecto_con_Angular/README.md
@@ -1,4 +1,4 @@
-# ProyectoArgProg
+# ProjectoConAngular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.1.
 
--- Projecto_con_Angular/src/app/app.component.spec.ts
+++ Projecto_con_Angular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ProyectoArgProg'`, () => {
+  it(`should have as title 'Projecto_con_Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ProyectoArgProg');
+    expect(app.title).toEqual('Projecto_con_Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ProyectoArgProg app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Projecto_con_Angular app is running!');
   });
 });
